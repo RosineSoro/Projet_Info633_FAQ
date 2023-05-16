e5348737d0e0539d59376f3454a7c7595ba5208a
Rosine Soro <gnidanhanrosine@gmail.com>
2023-05-16 16:30:49 +0200

    création des différents dossiers

---
+++ consultation/consultation.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+
+<html lang="fr"> 
+
+  
+
+   <head>
+		  <title>Page de Consultation</title>
+	
+		  <!--definition de l encodage-->
+		  <meta charset="UTF-8">
+		  
+		  <!--mise en lien du fichier html avec le fichier css-->
+		  <link rel="stylesheet" type="text/css" href="consultation.css">
+		  
+		  <!--recuperer l'icone de recherche-->
+	      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		  
+		 <!-- <i class="fa-sharp fa-light fa-star"></i> -->  
+		 
+    </head> 
+	
+	<body>
+	      <i class="fa-sharp fa-light fa-star"></i>
+	</body>
