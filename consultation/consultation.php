67466c7434bf00a005a848292eadd9a4738e7f89
unknown <sarahcadm@protonmail.com>
2023-05-16 22:17:56 +0200

    Ajout de login.php (lire README_login) & gestion de la session et deconnexion dans consultation.php /!\ PS: Il faudra probablemen changer le path dans 'header(Location: ...)'.

--- consultation/consultation.php
+++ consultation/consultation.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Gestion de la déconnexion
+if (isset($_POST['logout'])) {
+    session_unset(); // Supprimer toutes les variables de session
+    session_destroy(); // Détruire la session
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 
 
