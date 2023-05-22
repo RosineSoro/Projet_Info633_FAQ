f9bf59060a5f05f03c77f10f83914b6c431bba7d
Bob <113138769+chafikya@users.noreply.github.com>
2023-05-22 12:02:40 +0200

    [FEATURES] page modif

--- Pages/modif.php
+++ Pages/modif.php
@@ -26,22 +26,26 @@
         }
         ?>
 
-    
+<h1>Modifier les questions</h1>    
 <?php
     $id_Question=$_GET['id_question'];
     $sql="select * from question where id_question =".$id_Question;
     $result=  mysqli_query($conn, $sql);
     $ligne = mysqli_fetch_assoc($result);
-        echo ("<form method=\"post\">");
-    echo("<textarea name=\"titre\">".$ligne['titre']."</textarea>");
-    echo("<textarea name='question'>".$ligne['contenu']."</textarea>");
-    echo("<textarea name='reponse' placeholder='tapez votre reponse ici'></textarea>");
-    echo("<button type=\"submit\">valider</button>");
+        echo ("<form  method=\"post\">");
+    echo("<textarea class='modif' name=\"titre\">".$ligne['titre']."</textarea>");
+    echo("<textarea  class='modif' name='question'>".$ligne['contenu']."</textarea>");
+    echo("<textarea  class='modif' name='reponse' placeholder='tapez votre reponse ici'></textarea>");
+    echo("<div class='bouton' >");
+    echo("<button  type=\"submit\">valider</button>");
+    echo("</div>");
     echo("</form>");
     
 if (isset($_POST['reponse'])){
-    $sql="update question set titre =".$_POST['titre']. ",contenu=".$_POST['question']."where id_question=".$id_Question;
-    $sql1="update reponse set contenu_rep=".$_POST['reponse']."where id_question=".$id_Question;
+    $sql="update question set titre =".$_POST['titre']. ",contenu=".$_POST['question']."where id_question=".$id_Question
+    .";".
+    "update reponse set contenu_rep=".$_POST['reponse']."where id_question=".$id_Question;
+    $result=mysqli_query($conn, $sql);
 }
 
 ?>
