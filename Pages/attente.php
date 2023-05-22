6afc20d55051e05b7dc9ca353b89459c3ba88feb
ValentinPB <122006974+ValentinPB@users.noreply.github.com>
2023-05-22 11:37:06 +0200

    Infos questions attente
    Ajout des informations des questions en attente (contenu, catégorie, posteur), ainsi qu'un début de css.

--- Pages/attente.php
+++ Pages/attente.php
@@ -12,14 +12,25 @@
    </head> 
 
    <body>
+
+        <h1>Liste des questions en attente</h1>
+
         <?php
             $conn = @mysqli_connect("tp-epua:3308", "chafikya", "61md4vj3");
             mysqli_select_db($conn, "chafikya");
-            $sql = "SELECT id_question, titre FROM question WHERE verif = 0";
+            $sql = "SELECT id_question, titre, contenu, id_compte, id_cat FROM question WHERE verif = 0";
             $result = mysqli_query($conn, $sql) or die("Requête invalide: ". mysqli_error()."\n".$sql);
             $val = mysqli_fetch_array($result);
+            //catégorie
+            $sqlcat = "SELECT nom_cat FROM categorie WHERE id_Cat = ".$val['id_cat'];
+            $resultcat = mysqli_query($conn, $sqlcat) or die("Requête invalide: ". mysqli_error()."\n".$sqlcat);
+            $rescat = mysqli_fetch_array($resultcat);
+            //compte
+            $sqlacc = "SELECT nom, prenom FROM compte WHERE id_compte = ".$val['id_compte'];
+            $resultacc = mysqli_query($conn, $sqlacc) or die("Requête invalide: ". mysqli_error()."\n".$sqlacc);
+            $resacc = mysqli_fetch_array($resultacc);
             while ($val != FALSE) {
-                echo "<div class='question'><a href='modif.php?id_question=".$val['id_question']."'>".$val['titre']."</a></div>";
+                echo "<a href='modif.php?id_question=".$val['id_question']."'><div class='question'><h3 class='question_titre'>".$val['titre']."</h3><c>".$rescat['nom_cat']."</c><p>".$resacc['prenom']." ".$resacc['nom']."</p><t>".$val['contenu']."</t></div></a>";
                 $val = mysqli_fetch_array($result);
             }
         ?>
